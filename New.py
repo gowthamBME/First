7563164b4a7157c14949eb57c676c4b376a6c4ef
gowthamBME <gowthamkpm72@gmail.com>
2024-08-12 10:41:51 -0700

    My first commit

---
+++ New.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import signal
+t = np.linspace(0, 1, fs, endpoint=False)
+x = np.sin(2 * np.pi * 7 * t) + np.sin(2 * np.pi * 13 * t)
+x += 0.5 * np.random.randn(t.size)
+b, a = signal.butter(4, 10, fs=fs, btype='low')
+y = signal.filtfilt(b, a, x)
+plt.figure(figsize=(10, 6))
+plt.plot(t, x, label='Noisy Signal')
+plt.plot(t, y, label='Filtered Signal', linewidth=2)
+plt.legend()
+plt.xlabel('Time [seconds]')
+plt.ylabel('Amplitude')
+plt.title('Signal Filtering Example')
+plt.show()
